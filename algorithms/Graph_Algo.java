f07333a08767f304ed93a97d62f6e28e69a5b99b
op1995 <pellerofir@gmail.com>
2019-12-25 22:52:14 +0200

    implemented TSP - NOT YET TESTED. SHOULD BE TESTED.

--- algorithms/Graph_Algo.java
+++ algorithms/Graph_Algo.java
@@ -31,16 +31,16 @@ public class Graph_Algo implements graph_algorithms, Serializable{
 	 * 
 	 */
 	public DGraph Graph;
-	
+
 	@Override
 	public void init(graph g) {
 		this.Graph = (DGraph) g;
 	}
-	
+
 	public void init(Graph_Algo g) {
 		this.Graph = (DGraph) g.copy();
 	}
-	
+
 	@Override
 	public graph copy() {
 		DGraph copy = new DGraph();
@@ -58,15 +58,15 @@ public class Graph_Algo implements graph_algorithms, Serializable{
 	public void save(String file_name) {
 		try {
 			FileOutputStream file = new FileOutputStream(file_name); 
-	        ObjectOutputStream out;
+			ObjectOutputStream out;
 			out = new ObjectOutputStream(file);
 			DGraph graphCopy = (DGraph)this.copy();
 			Graph_Algo tempToFile = new Graph_Algo();
 			tempToFile.init(graphCopy);
-	        out.writeObject(tempToFile); 
-	        out.close(); 
-	        file.close();
-	        System.out.println("Object has been serialized"); 
+			out.writeObject(tempToFile); 
+			out.close(); 
+			file.close();
+			System.out.println("Object has been serialized"); 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -75,27 +75,27 @@ public class Graph_Algo implements graph_algorithms, Serializable{
 	}
 	@Override
 	public void init(String file_name) {
-        try
-        {    
-            FileInputStream file = new FileInputStream(file_name); 
-            ObjectInputStream in = new ObjectInputStream(file); 
-              
-            init((Graph_Algo)in.readObject());
-            in.close(); 
-            file.close(); 
-              
-            System.out.println("Object has been deserialized"); 
-        } 
-          
-        catch(IOException ex) 
-        { 
-            System.out.println("IOException is caught"); 
-        } 
-          
-        catch(ClassNotFoundException ex) 
-        { 
-            System.out.println("ClassNotFoundException is caught"); 
-        } 
+		try
+		{    
+			FileInputStream file = new FileInputStream(file_name); 
+			ObjectInputStream in = new ObjectInputStream(file); 
+
+			init((Graph_Algo)in.readObject());
+			in.close(); 
+			file.close(); 
+
+			System.out.println("Object has been deserialized"); 
+		} 
+
+		catch(IOException ex) 
+		{ 
+			System.out.println("IOException is caught"); 
+		} 
+
+		catch(ClassNotFoundException ex) 
+		{ 
+			System.out.println("ClassNotFoundException is caught"); 
+		} 
 	}
 	@Override
 	public boolean isConnected() {
@@ -107,11 +107,11 @@ public class Graph_Algo implements graph_algorithms, Serializable{
 		Node lastNodeInPath = (Node) temp.get(temp.size());
 		return lastNodeInPath.getWeight();
 	}
-	
-	
+
+
 	@Override
 	public List<node_data> shortestPath(int src, int dest) {
-//		first check if the two nodes are connected
+		//		first check if the two nodes are connected
 		HashMap<Integer,Boolean> visited1 = new HashMap<Integer,Boolean>();
 		for(int u : this.Graph.getNodes().keySet()) {
 			visited1.put(u,false);
@@ -120,7 +120,7 @@ public class Graph_Algo implements graph_algorithms, Serializable{
 		if(!(visited1.get(dest))) {
 			throw new RuntimeException("There is no existing path between the source and the destination!");
 		}
-		
+
 		Node source = (Node) this.Graph.getNode(src);
 		source.setWeight(0);
 		source.predecessor = null;
@@ -132,25 +132,25 @@ public class Graph_Algo implements graph_algorithms, Serializable{
 			graphNodes.get(i).predecessor = null;
 			myHeap.add(graphNodes.get(i));
 		}
-		
-		
-		
+
+
+
 		while(!myHeap.isEmpty()) {
 			Node currentNode = myHeap.heapExtractMin();
 			int currentKey = currentNode.getKey();
-			
+
 			for(int i : this.Graph.getEdges().get(currentKey).keySet()) {
 				Node currentNextNode = (Node) this.Graph.getNode(i);
 				if(!currentNextNode.visited) {
 
-					
+
 					double currentEdgeWeight = this.Graph.getEdges().get(currentKey).get(i).getWeight();
 					double optionalNewDistance = currentNode.getWeight() + currentEdgeWeight;
-					
+
 					if(optionalNewDistance<currentNextNode.getWeight()) {
 						currentNextNode.setWeight(optionalNewDistance);
 						currentNextNode.predecessor = currentNode;
-						
+
 						for (int j = 0; j < myHeap.size; j++) { //update the minHeap
 							if(myHeap.heapNodeArray[j]==currentNextNode) {
 								myHeap.minHeapify((j-1)/2); //heapify the parent node
@@ -169,13 +169,15 @@ public class Graph_Algo implements graph_algorithms, Serializable{
 			currentNode2 = currentNode2.predecessor;
 		}
 		Collections.reverse(Answer);
-		
-		
+
+
 		return Answer;
 	}
-	
+
 	@Override
 	public List<node_data> TSP(List<Integer> targets) {
+		double minimum = Double.POSITIVE_INFINITY;
+		ArrayList<Integer> minimumNodeKeyList = new ArrayList<Integer>();
 		HashMap<Integer, HashMap<Integer,Double>> Distances = new HashMap<Integer, HashMap<Integer,Double>>();
 		for (int i : targets) {
 			Distances.put(i, new HashMap<Integer,Double>()); //create a new inner HashMap for each of the targets
@@ -184,11 +186,70 @@ public class Graph_Algo implements graph_algorithms, Serializable{
 				double currentDistance = shortestPathDist(i,j);
 				Distances.get(i).put(j, currentDistance);
 			}
+		}//end calculate all ShortestPath of all targets to each other
+
+		for (int i : targets) {
+			int[] allTargetssBesideBeginningNode = new int[targets.size()-1];
+			int k = 0;
+			for (int j: targets) {
+				if(i!=j) {//if this node isn't the beginning node
+					allTargetssBesideBeginningNode[k]=j;
+					k++;
+				}
+			}
+			ArrayList<ArrayList<Integer>> currentPermutations = new ArrayList<ArrayList<Integer>>();
+			currentPermutations = permuteFatherMethod(allTargetssBesideBeginningNode);
+			for (ArrayList<Integer> specificPermutation : currentPermutations) {
+				double currentMinimum = Distances.get(i).get(specificPermutation.get(0)); //add the 1st journey cost
+				for (int j = 1; j < specificPermutation.size(); j++) {
+					double addToCurrentMinimum = Distances.get(specificPermutation.get(j-1)).get(specificPermutation.get(j)); //add the next journey
+					currentMinimum = currentMinimum + addToCurrentMinimum; //add the next journey cost to the total cost
+				}
+				if (currentMinimum<minimum) { //test if the now found trip is the cheapest, up till now.
+					minimum = currentMinimum;
+					specificPermutation.add(0, i); //add the key of the Node we begin at, to the list.
+					minimumNodeKeyList = specificPermutation;
+				}
+			}//end iterate over permutations of current beginning node
+		}//end iterate, changing beginning node.
+		
+		
+		List <node_data> answer = new ArrayList<node_data>();
+		for (int i = 0; i < minimumNodeKeyList.size(); i++) {
+			answer.add(this.Graph.getNode(minimumNodeKeyList.get(i)));
 		}
-		
-		
-		return null;
+		return answer;
+	}//end TSP
+
+	public ArrayList<ArrayList<Integer>> permuteFatherMethod(int[] arr) {
+		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+		permuteHelper(list, new ArrayList<>(), arr);
+		return list;
 	}
+
+	private void permuteHelper(ArrayList<ArrayList<Integer>> list, ArrayList<Integer> resultList, int [] arr){
+		// Base case
+		if(resultList.size() == arr.length){
+			list.add(new ArrayList<>(resultList));
+		} 
+		else{
+			for(int i = 0; i < arr.length; i++){ 
+
+				if(resultList.contains(arr[i])) 
+				{
+					// If element already exists in the list then skip
+					continue; 
+				}
+				// Choose element
+				resultList.add(arr[i]);
+				// Explore
+				permuteHelper(list, resultList, arr);
+				// Unchoose element
+				resultList.remove(resultList.size() - 1);
+			}
+		}
+	}
+
 	public void DFS(DGraph g, int v, HashMap<Integer,Boolean> visited) {
 		if(visited.containsKey(v)) {
 			visited.replace(v, true);
@@ -216,68 +277,68 @@ public class Graph_Algo implements graph_algorithms, Serializable{
 		}
 		return true;
 	}
-//	public static boolean check(DGraph graph)
-//	{
-//		// stores vertex is visited or not
-//		HashMap<Integer,Boolean> visited = new HashMap<Integer,Boolean>();
-//		for (int i : graph.getNodes().keySet()) {
-//			visited.put(i, false);
-//		}// choose random starting point
-//		Set<Integer> NodeList = graph.getNodes().keySet();
-//		Object[] arr = NodeList.toArray();
-//		int v = (int) arr[0];
-//		
-//		// run a DFS starting at v
-//		DFS(graph, v, visited);
-//
-//		// If DFS traversal doesn’t visit all vertices,
-//		// then graph is not strongly connected
-//		Set<Integer> KeySet;
-//		KeySet = visited.keySet();
-//		for (int b: KeySet)
-//			if (!visited.get(b))
-//				return false;
-//	
-//		for (int i : graph.getNodes().keySet()) {
-//			visited.put(i, false);
-//		}
-//		HashMap<Integer, HashMap<Integer, Edge_Data>> edges = new HashMap<Integer, HashMap<Integer, Edge_Data>>();
-//		for(int u : NodeList) {
-//			Set<Integer> EdgeList = graph.getEdge(u).keySet();
-//			for (int z : EdgeList) {
-//				HashMap<Integer, Edge_Data> value = new HashMap<Integer, Edge_Data>();
-//				value.put(u,(Edge_Data) graph.getEdge(u, z));
-//				edges.put(z,value);
-//			}
-//		}
-//		DGraph gr = new DGraph(graph.getNodes(),edges);
-//		// Again run a DFS starting at v
-//		DFS(gr, v, visited);
-//
-//		// If DFS traversal doesn’t visit all vertices,
-//		// then graph is not strongly connected
-//		Set<Integer> KeySet2;
-//		KeySet2 = visited.keySet();
-//		for (int b: KeySet2)
-//			if (!visited.get(b))
-//				return false;
-//
-//		// if graph "passes" both DFSs, it is strongly connected
-//		return true;
-//	}
-//	private static Object deepCopy(Object object) {
-//	   try {
-//	     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//	     ObjectOutputStream outputStrm = new ObjectOutputStream(outputStream);
-//	     outputStrm.writeObject(object);
-//	     ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
-//	     ObjectInputStream objInputStream = new ObjectInputStream(inputStream);
-//	     return objInputStream.readObject();
-//	   }
-//	   catch (Exception e) {
-//	     e.printStackTrace();
-//	     return null;
-//	   }
-// }
-	
+	//	public static boolean check(DGraph graph)
+	//	{
+	//		// stores vertex is visited or not
+	//		HashMap<Integer,Boolean> visited = new HashMap<Integer,Boolean>();
+	//		for (int i : graph.getNodes().keySet()) {
+	//			visited.put(i, false);
+	//		}// choose random starting point
+	//		Set<Integer> NodeList = graph.getNodes().keySet();
+	//		Object[] arr = NodeList.toArray();
+	//		int v = (int) arr[0];
+	//		
+	//		// run a DFS starting at v
+	//		DFS(graph, v, visited);
+	//
+	//		// If DFS traversal doesn’t visit all vertices,
+	//		// then graph is not strongly connected
+	//		Set<Integer> KeySet;
+	//		KeySet = visited.keySet();
+	//		for (int b: KeySet)
+	//			if (!visited.get(b))
+	//				return false;
+	//	
+	//		for (int i : graph.getNodes().keySet()) {
+	//			visited.put(i, false);
+	//		}
+	//		HashMap<Integer, HashMap<Integer, Edge_Data>> edges = new HashMap<Integer, HashMap<Integer, Edge_Data>>();
+	//		for(int u : NodeList) {
+	//			Set<Integer> EdgeList = graph.getEdge(u).keySet();
+	//			for (int z : EdgeList) {
+	//				HashMap<Integer, Edge_Data> value = new HashMap<Integer, Edge_Data>();
+	//				value.put(u,(Edge_Data) graph.getEdge(u, z));
+	//				edges.put(z,value);
+	//			}
+	//		}
+	//		DGraph gr = new DGraph(graph.getNodes(),edges);
+	//		// Again run a DFS starting at v
+	//		DFS(gr, v, visited);
+	//
+	//		// If DFS traversal doesn’t visit all vertices,
+	//		// then graph is not strongly connected
+	//		Set<Integer> KeySet2;
+	//		KeySet2 = visited.keySet();
+	//		for (int b: KeySet2)
+	//			if (!visited.get(b))
+	//				return false;
+	//
+	//		// if graph "passes" both DFSs, it is strongly connected
+	//		return true;
+	//	}
+	//	private static Object deepCopy(Object object) {
+	//	   try {
+	//	     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+	//	     ObjectOutputStream outputStrm = new ObjectOutputStream(outputStream);
+	//	     outputStrm.writeObject(object);
+	//	     ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+	//	     ObjectInputStream objInputStream = new ObjectInputStream(inputStream);
+	//	     return objInputStream.readObject();
+	//	   }
+	//	   catch (Exception e) {
+	//	     e.printStackTrace();
+	//	     return null;
+	//	   }
+	// }
+
 }
